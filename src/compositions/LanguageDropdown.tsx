9e5441579465fea97b3702c0f018966827149d40
Nils Kuechler <nils@ukuechler.de>
2025-07-01 12:48:33 +0100

    feat(components): improve visibility of dropdown

--- src/compositions/LanguageDropdown.tsx
+++ src/compositions/LanguageDropdown.tsx
@@ -2,15 +2,15 @@ import { useUserSettingsFormContext } from "../hooks/useUserSettingsFormContext"
 import Dropdown from "../components/dropdown/Dropdown";
 
 const languageOptions = [
-	{ value: "option1", label: "Option 1" },
-	{ value: "option2", label: "Option 2" },
-	{ value: "option3", label: "Option 3" },
+	{ value: "English", label: "English" },
+	{ value: "German", label: "German" },
+	{ value: "French", label: "French" },
 ];
 
 export function LanguageDropdown() {
 	const {
 		handleDropdownChange,
-		form: { language: value = "option1" },
+		form: { language: value = "English" },
 		saveStatus,
 	} = useUserSettingsFormContext();
 	return (
@@ -18,7 +18,7 @@ export function LanguageDropdown() {
 			options={languageOptions}
 			value={value}
 			onChange={handleDropdownChange}
-			label="Select an Option"
+			label="Select a Language"
 			disabled={saveStatus === "saving"}
 		/>
 	);
